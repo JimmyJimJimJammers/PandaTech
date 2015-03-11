022223047c3473a9842bde955b9d6db86e9627a7
Miguel Hummel <miguelhummel@Miguels-Mac.local>
2015-03-11 15:31:37 -0400

    -Tesseract Library imported and bridged to Swift -backend OCR code not implemented yet (will need to also import GPUImage)

---
+++ SDD_Bill_It/SDD_Bill_It-Bridging-Header.h
@@ -0,0 +1,6 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+//imports the Tesseract library from Obj-C to Swift
+#import <TesseractOCR/TesseractOCR.h>
